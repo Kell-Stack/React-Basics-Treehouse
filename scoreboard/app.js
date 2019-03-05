984ee4271af97f20002bac8f3d68c7c71a0f276b
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-03-05 15:20:13 -0800

    added arrow function example but will revert back to prev way to follow vid for now

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,13 +1,23 @@
-function Header(){                                      //define the component as either a JavaScript function or class. our Header function is defining a component
-    return (                                            //note: React components are required to begin with an upper case letter
+// function Header(){                                      //define the component as either a JavaScript function or class. our Header function is defining a component
+//     return (                                            //note: React components are required to begin with an upper case letter
+//         <header>
+//             <h1>Scoreboard</h1>
+//             <span className="stats"> Players: 1 </span>
+//         </header>
+//     )
+// }
+
+const Header2 = ()  =>                                                                               
         <header>
             <h1>Scoreboard</h1>
             <span className="stats"> Players: 1 </span>
-        </header>
-    )
-}
+        </header>;
+
+
     
-    ReactDOM.render(                    //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
-        <Header />,                     //first arg: the React element or JS object that describes the element you'd like to render
-        document.getElementById('root') //second arg: the actual HTML element you want to update or render to
+ReactDOM.render(                    //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
+//   <Header />,                    //first arg: the React element or JS object that describes the element you'd like to render
+  <Header2 />, 
+  document.getElementById('root')   //second arg: the actual HTML element you want to update or render to
+
     );       
