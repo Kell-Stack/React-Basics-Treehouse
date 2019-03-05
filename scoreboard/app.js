886f4ce2a6afdb9eb9738460eb8e3185aa05f02e
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-03-05 15:08:26 -0800

    created first component header for scoreboard

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,20 +1,13 @@
-const title = 'My First React Element!!!';   
-
-const desc = 'I just learned how react works!';
-
-const myTitleID = 'main-title';     //JSX Expression: the value of an attribute in the JSX tag
-
-const name = "Kellay";
-
-const header = (
-    <header>
-        <h1 id = {myTitleID} > {name}'s First React Element' </h1>      {/* JSX Expression: the value of an attribute in the JSX tag */}
-        <p className = ""> {desc} </p>
+function Header(){                                      //define the component as either a JavaScript function or class. our Header function is defining a component
+    return (                                            //note: React components are required to begin with an upper case letter
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats"> Players: 1 </span>
+        </header>
+    )
+}
     
-    </header>
-);
-
-ReactDOM.render( //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
-    header, //first arg: the React element or JS object that describes the element you'd like to render
-    document.getElementById('root') //second arg: the actual HTML element you want to update or render to
-);      
+    ReactDOM.render(                    //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
+        <Header />,                     //first arg: the React element or JS object that describes the element you'd like to render
+        document.getElementById('root') //second arg: the actual HTML element you want to update or render to
+    );       
