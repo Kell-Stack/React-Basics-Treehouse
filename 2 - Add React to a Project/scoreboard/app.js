69bf136d05998639fa3703beea0043b6eb0f5507
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-03-05 10:03:55 -0800

    first commit downloaded files from treehouse, working and annotating first project

---
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -0,0 +1,7 @@
+const title = React.createElement(
+    'h1',
+){id: 'main-title', title: 'This is a title.')},
+'My First React Element!!!'  //create elements accepts three arguments that define the element you want to create
+):
+
+console.log(title);
