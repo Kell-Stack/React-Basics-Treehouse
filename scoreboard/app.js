58165304b410ef10b2c63f7c7bd0730f509dfe78
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-03-05 15:55:35 -0800

    composing components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,23 +1,46 @@
-// function Header(){                                      //define the component as either a JavaScript function or class. our Header function is defining a component
-//     return (                                            //note: React components are required to begin with an upper case letter
-//         <header>
-//             <h1>Scoreboard</h1>
-//             <span className="stats"> Players: 1 </span>
-//         </header>
-//     )
-// }
-
-const Header2 = ()  =>                                                                               
+function Header(){                                      //define the component as either a JavaScript function or class. our Header function is defining a component
+    return (                                            //note: React components are required to begin with an upper case letter
         <header>
             <h1>Scoreboard</h1>
             <span className="stats"> Players: 1 </span>
-        </header>;
+        </header>
+    )
+}
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Kelly
+            </span>
+            <Counter />
+        </div>
+    );
+} 
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">100</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return(
+        <div className="scoreboard">
+        <Header />
+
+    {/*Players list*/}
+    <Player />
+        </div>
+    )
+}
 
     
-ReactDOM.render(                    //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
-//   <Header />,                    //first arg: the React element or JS object that describes the element you'd like to render
-  <Header2 />, 
-  document.getElementById('root')   //second arg: the actual HTML element you want to update or render to
-
+    ReactDOM.render(                    //connects React to the DOM. the function that actually does the creating and updating of the DOM. accepts two args
+        <App />,
+        document.getElementById('root')
     );       
